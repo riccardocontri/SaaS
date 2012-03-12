74e15893726a26423b29b2b185635b57315d373c
Riccardo Contri <riccardocontri@libero.it>
2012-03-11 17:23:30 -0700

    Hw2.3 Sort movie list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all :order => params[:order_by]
+    @ordered_by = params[:order_by]
   end
 
   def new
